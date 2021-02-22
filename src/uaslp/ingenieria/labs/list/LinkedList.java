addaa13795fbece027d5b5767a564e1111fdcc50
Ivan Uresti <ivan.uresti@gmail.com>
2021-02-22 08:59:01 -0600

    Añadir método add

--- src/uaslp/ingenieria/labs/list/LinkedList.java
+++ src/uaslp/ingenieria/labs/list/LinkedList.java
@@ -1,4 +1,42 @@
 package uaslp.ingenieria.labs.list;
 
+/**
+ * Lista doblemente ligada
+ */
 public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    /**
+     * Inserts data at the end of the list
+     * @param data Data to be inserted
+     */
+    public void add(int data){
+        Node node = new Node(data);
+
+        node.setPrevious(tail);
+
+        if(tail != null){
+            tail.setNext(node);
+        }
+
+        if(head == null){
+            head = node;
+        }
+
+        tail = node;
+        size++;
+    }
+
+    //get por índice
+
+    //delete
+
+    //Iterador -> patrón de diseño
+
+    public int getSize() {
+        return size;
+    }
 }
